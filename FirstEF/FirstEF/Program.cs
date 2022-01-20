05425981d753caba7f1fc7c4417d742d393e83c4
kbodyan <kbodyan@gmail.com>
2022-01-20 22:41:58 +0200

    empty_project

---
+++ FirstEF/FirstEF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using EFCore
+
+namespace FirstEF
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new C
+        }
+    }
+}
